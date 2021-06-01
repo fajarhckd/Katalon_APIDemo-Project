6d4323a08e7b8acb8b91e5df9d2523ccca256a38
fajarhckd <fajarhdytt30@gmail.com>
2021-06-01 19:42:14 +0700

    first project commit

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>45672487-fc71-4b35-8a1d-291f860067e0</testSuiteGuid>
+   <testCaseLink>
+      <guid>5245a1df-6929-4326-b07f-76e59cf13a22</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>49c6a600-024b-4d2d-aa59-6c8b738a9c37</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestHTTPMethods</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>503fe8cd-299e-439a-959f-f19bebe41481</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTest/Addition</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
